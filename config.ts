39285cf184f8aa9b67f326c811b6654f7f13421a
hzeroo <edusvh@gmail.com>
2019-04-24 17:36:53 +0200

    Added config.ts with client-id token to allow aur packaging

---
+++ config.ts
@@ -0,0 +1,18 @@
+export default {
+  client_id: "ep54r07ax1ksni0f84caon2epc4dlbh",
+
+  schema: {
+    betterttv: {
+      type: 'boolean',
+      default: true
+    },
+    autologin: {
+      type: 'boolean',
+      default: true
+    },
+    client_id: {
+      type: 'string',
+      default: ''
+    }
+  }
+}
